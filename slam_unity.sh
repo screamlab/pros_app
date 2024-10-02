48df273a07edbb8adcf17ecfc8857d05708f68cf
otischung <my072814638@gmail.com>
2024-10-02 17:34:58 +0800

    [REFACTOR] Use utils.sh to rewrite the shell scripts

--- slam_unity.sh
+++ slam_unity.sh
@@ -1,30 +1,4 @@
 #!/bin/bash
 
-# Determine which docker compose command to use
-if command -v docker-compose &> /dev/null
-then
-    DOCKER_COMPOSE_COMMAND="docker-compose"
-elif docker compose version &> /dev/null
-then
-    DOCKER_COMPOSE_COMMAND="docker compose"
-else
-    echo "Neither 'docker-compose' nor 'docker compose' is installed. Please install Docker Compose."
-    exit 1
-fi
-
-$DOCKER_COMPOSE_COMMAND -f ./scripts/docker-compose_rplidar_unity.yml up -d
-$DOCKER_COMPOSE_COMMAND -f ./scripts/docker-compose_slam_unity.yml up -d
-
-cleanup() {
-    echo "Shutting down docker compose services..."
-    docker ps -aq --filter "name=scripts*" | xargs docker rm -f
-    exit 0
-}
-
-trap cleanup SIGINT
-
-echo "Press Ctrl+C to stop..."
-
-echo "Listening to docker-compose_slam_unity.yml logs. Press Ctrl+C to stop..."
-$DOCKER_COMPOSE_COMMAND -f ./scripts/docker-compose_slam_unity.yml logs -f &
-wait
+source "./utils.sh"
+main "./scripts/docker-compose_rplidar_unity.yml" "./scripts/docker-compose_slam_unity.yml"
