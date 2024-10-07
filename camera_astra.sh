e3b695744a91abe90b4424e5680bf9522cdd51e3
otischung <my072814638@gmail.com>
2024-10-07 20:53:43 +0800

    [REFACTOR] Rename camera yaml file

--- camera_astra.sh
+++ camera_astra.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source "./utils.sh"
-main "./scripts/docker-compose_camera.yml"
+main "./scripts/docker-compose_camera_astra.yml"
--- camera_dabai.sh
+++ camera_dabai.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source "./utils.sh"
-main "./scripts/docker-compose_dabai_camera.yml"
+main "./scripts/docker-compose_camera_dabai.yml"
